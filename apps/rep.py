df8621da3e7c336a48b55cb663ef32b81604a6f9
Angelina Li <ali6@wellesley.edu>
2018-08-30 01:11:44 +0200

    updated dashboard; broke captions

--- apps/rep.py
+++ apps/rep.py
@@ -43,7 +43,6 @@ class Tab(dcc.Tab):
 dashboard = html.Div(
   className="col-sm-3",
   children=[
-    html.Br(),
     html.H5("Change what this figure shows below:"),
     html.Br(),
     utils.FormElement(
@@ -71,6 +70,17 @@ dashboard = html.Div(
         id="years",
         values=range(2002, 2019)
       )
+    ),
+    utils.FormElement(
+      label="Comparison Specificity",
+      element=dcc.Dropdown(
+        id="race",
+        options=[
+          {"label": "POC / Non-POC", "value": "poc_flag"},
+          {"label": "All categories", "value": "all"}
+        ],
+        value="poc_flag"
+      )
     )
   ]
 )
@@ -82,7 +92,7 @@ overall_tab = Tab(
   value="overall", 
   children=[
     html.Div(id="overall-value", style=dict(display="none")),
-    html.H2("People of Color on the Bachelor/ette"),
+    html.H3("People of Color on the Bachelor/ette"),
     dcc.Graph(id="overall-bar"),
     dcc.Markdown(id="overall-caption")
   ]
@@ -91,13 +101,17 @@ overall_tab = Tab(
 evolution_tab = Tab(
   label="Evolution", 
   value="evolution",
-  children=[]
+  children=[
+    html.H3("The representation of POC has been improving over time")
+  ]
 )
 
 comparison_tab = Tab(
   label="Comparison", 
   value="comparison",
-  children=[]
+  children=[
+    html.H3("The Bachelor/ette is still less diverse than the U.S. at large")
+  ]
 )
 
 panel = html.Div(
@@ -124,28 +138,39 @@ layout = utils.BSContainer(
 
 ########## interactive routes ##########
 
+# objective: get a cleaned dataframe based on specifications. We want data on
+# the number of people within different racial categories for each of the lead
+# values.
 @app.callback(
   Output("overall-value", "children"),
   [Input(input_id, "value") for input_id in 
-    ["leads", "shows", "years"] ]
+    ["leads", "shows", "years", "race"] ]
 )
-def clean_overall_data(leads, shows, years):
+def clean_overall_data(leads, shows, years, race):
   filtered_df = df[
     (df["race_data_flag"] == True)
     & (df["show"].isin(shows))
-    * (df["year"].isin(years)) ][
-    ["lead_flag", "race_data_flag", "poc_flag"]
+    * (df["year"].isin(years))
   ]
-  groupings = {}
-  poc_vals = [True, False]
-  for poc_val in poc_vals:
-    poc_series = filtered_df[df["poc_flag"] == poc_val]["lead_flag"]
-    poc_series = poc_series[poc_series.isin(leads)] \
-                 .map(lambda x: {True: "Leads", False: "Contestants"}[x])
-    counter = Counter(poc_series)
-    group_vars = {"x": list(counter.keys()), "y": list(counter.values())}
-    groupings[poc_val] = group_vars
-  return json.dumps(groupings)
+  lead_data = {}
+  for lead in leads:
+    lead_df = filtered_df[filtered_df["lead_flag"] == lead]
+    if race == "poc_flag":
+      poc_series = lead_df["poc_flag"] \
+                   .map(lambda x: {True: "POC", False: "White"}[x])
+      counter = Counter(poc_series)
+      lead_data[lead] = dict(counter)
+    # when we want disaggregated race categories
+    else:
+      race_flags = ["white", "afam", "amin", "hisp", "asn_paci", "oth", "mult"]
+      race_titles = ["White", "African American", "American Indian", "Hispanic",
+                     "Asian & Pacific Islander", "Other", "Multi"]
+      race_title_dict = dict(zip(race_flags, race_titles))
+      counts = lead_df.count()
+      counter = {race_title_dict.get(race_flag): int(counts[race_flag]) 
+                 for race_flag in race_flags}
+      lead_data[lead] = dict(counter)
+  return json.dumps(lead_data)
 
 @app.callback(
   Output("overall-bar", "figure"),
@@ -153,23 +178,27 @@ def clean_overall_data(leads, shows, years):
 )
 def update_overall_tab(cleaned_data):
   """ generates figure for overall tab """
-  groupings = json.loads(cleaned_data)
-  poc_vals = ["true", "false"]
-  colors = dict(zip(poc_vals, utils.COLORSCHEME))
+  data = json.loads(cleaned_data)
+  if not data:
+    return
+  groupings = list(data.values())[0].keys()
+  colors = dict(zip(groupings, utils.COLORSCHEME))
   traces = []
-  for poc_val, group_vars in groupings.items():
+  for val in groupings:
+    x_init = data.keys()
+    y = [data.get(x).get(val) for x in x_init]
+    x = list(map(lambda x: {"true": "Leads", "false": "Contestants"}[x], x_init))
     strat_bar = go.Bar(
-      x=group_vars.get("x"),
-      y=group_vars.get("y"),
+      x=x,
+      y=y,
       hoverinfo="x+y",
-      marker=dict(color=colors.get(poc_val, utils.PRIMARY_COLOR)),
-      name="POC" if poc_val == "true" else "White"
+      marker=dict(color=colors.get(val, utils.PRIMARY_COLOR)),
+      name=val
     )
     traces.append(strat_bar)
   layout = go.Layout(
     xaxis=dict(tickfont=dict(size=14)),
     yaxis=dict(title="Number of People", titlefont=dict(size=16)),
-    hovermode="closest"
   )
   return dict(data=traces, layout=layout)
 
